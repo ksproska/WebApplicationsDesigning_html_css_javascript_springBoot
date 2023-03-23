d35940bd2db4464104795d97805829d1618e8b5a
Kamila Sproska <kamila.sproska@gmail.com>
2023-03-23 08:29:18 +0100

    list 2 exercise 1 finished

--- lista2/zad-1/script.js
+++ lista2/zad-1/script.js
@@ -1,14 +1,15 @@
 function onSliderChange(value) {
-    const circle = document.getElementById("circle");
+    const circle = document.getElementById("circle")
     circle.style.height = '' + value + 'px'
     circle.style.lineHeight = '' + value + 'px'
     circle.style.width = '' + value + 'px'
     circle.style.borderRadius = '' + (value * 2) + 'px'
-    const center = document.getElementById("center");
-    center.innerText='' + value + 'px'
+    circle.innerText='' + value + 'px'
 }
 
-// const slider = document.getElementById("myRange");
-// slider.oninput = function () {
-//     onSliderChange(this.value)
-// }
+window.onload = (event) => {
+    const slider = document.getElementById("slider")
+    let initValue = 10
+    slider.value = initValue
+    onSliderChange(initValue)
+}
